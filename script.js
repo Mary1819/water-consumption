822da7deb804e873291992fa532f0f71c7b12cd5
Glitch (sd-project) <none>
2024-01-10 01:55:13 +0000

    🩰🔒 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,41 @@
+// first Answer
+$(".firYes").click(function () {
+  $(".firPag").text("Nice, you saved 3-4 gallons of the 8 gallons of water you use to brush your teeth by turning the water off when brushing your teeth. To save more water, use a cup when rinsing.");
+});
+$(".firNo").click(function () {
+  $(".firPag").text("You wasted 3-4 gallons of the 8 gallons of water you use to brush your teeth daily by not turning the faucet off when brushing your teeth. Turn the faucet off as often as possible when brushing and use a cup when rising. ");
+});
+
+// second Answer
+$(".secYes").click(function () {
+  $(".secPag").text("You wasted up to 50 gallons of water by taking a bath. You don’t need to stop taking a bath to save water. Try to take baths occasionally, filling the tub part way, and showering instead of a bath.");
+});
+$(".secNo").click(function () {
+  $(".secPag").text("Nice, you saved up to 50 gallons of water by not taking a bath. Taking baths occasionally, filling the tub part way, and showering instead of a bath few better options.");
+});
+
+// third Answer
+$(".thiYes").click(function () {
+  $(".thiPag").text("You wasted more than 25 gallons of water by showering for more than 10 minutes. You can save water by taking shorter showers, using water-saving shower heads, and turning the water off when not rinsing.");
+});
+$(".thiNo").click(function () {
+  $(".thiPag").text("Nice, you used no more than 25 gallons of water by showering in less than 10 minutes. Ways to save more water are by using water-saving shower heads, and turning the water off when not rinsing.");
+});
+
+// fourth Answer
+$(".fouYes").click(function () {
+  $(".fouPag").text("By letting your faucet run for 5 minutes while washing dishes can waste 10 gallons of water. Some ways to save water while washing dishes is by using warm water to remove waste quickly and using the correct amount of detergent/soap.");
+});
+$(".fouNo").click(function () {
+  $(".fouPag").text("You saved water by not letting your faucet run when scrubbing. You can save more water by keeping the dishes closer before turning the water on and using warm water to remove waste quickly.");
+});
+
+// fifth Answer
+$(".fifYes").click(function () {
+  $(".fifPag").text("Nice, you wasted less water by running the dishwasher when it’s full than when it only has 4 dishes, which is about 7-12 gallons of water. If you scrape the solids off the dishes before putting them in the dishwasher, the pre-rinse could be skipped. Additionally, since leaks are frequent and cause more water loss than consumption, maintain and replace your dishwasher as necessary.");
+});
+$(".fifNo").click(function () {
+  $(".fifPag").text("You wasted the same amount of water when a full load and 4 dishes are getting washed by a dishwasher, which is about 7-12 gallons of water. Run your dishwasher only when it’s full, even if you have to wait days till it gets full. If you can’t be patient till it gets full, hand-washing is another option. If you scrape the solids off the dishes, the pre-rinse step could be skipped.");
+});
+
+
